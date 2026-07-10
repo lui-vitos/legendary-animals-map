47bc9d655386b7a55446bda20bd01bb3c44ceba3
lui-vitos <vit2811@mail.ru>
2026-07-10 10:08:06 +0300

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,7 @@
+// Yandex Metrika counter ID.
+// 1) Create a counter at https://metrika.yandex.ru/
+// 2) Replace 0 below with your counter number (digits only).
+// 3) Upload this file to GitHub together with other site files.
+//
+// Example: window.YANDEX_METRIKA_ID = 12345678;
+window.YANDEX_METRIKA_ID = 110553702;
